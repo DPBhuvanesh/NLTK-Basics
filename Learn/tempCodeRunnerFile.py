ec1a7e128f22d6a00fa7b9ec0404513f60ceeb20
DPBhuvanesh <113875447+DPBhuvanesh@users.noreply.github.com>
2024-01-30 21:24:01 +0530

    Delete Learn/tempCodeRunnerFile.py

--- Learn/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-from nltk.stem import PorterStemmer #portstemmer used for stemming
-
-p=PorterStemmer()  #assign porterstemmer for p
-
-text=['simbu','simbooo','simbi','simbee']
-
-for i in text:
-    print(p.stem(i))
